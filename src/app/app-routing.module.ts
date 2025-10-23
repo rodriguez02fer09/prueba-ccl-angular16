1d508baf4d2c176b9d0de6520c1bbc21a26be5f8
Fernanda Rodriguez <fernandarpy@gmail.com>
2025-10-22 20:43:57 -0500

    feat(inventario): agrega listado de productos con buscador y carga desde mock JSON

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core'
 import {RouterModule, Routes} from '@angular/router'
 import {LoginComponent} from './auth/components/login/login.component'
 import {DashboardComponent} from './features/dashboard/dashboard.component'
+import {InventarioComponent} from './features/inventario/inventario.component'
 
 const routes: Routes = [
   {path: '', redirectTo: 'login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'dashboard', component: DashboardComponent},
+  {path: 'consultar', component: InventarioComponent},
   {path: '**', redirectTo: 'login'},
 ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,20 @@ import {MatCardModule} from '@angular/material/card'
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatInputModule} from '@angular/material/input'
 import {MatButtonModule} from '@angular/material/button'
-
+import {FormsModule} from '@angular/forms'
 import {AppRoutingModule} from './app-routing.module'
 import {AppComponent} from './app.component'
-import {LoginComponent} from './auth/components/login/login.component';
-import { DashboardComponent } from './features/dashboard/dashboard.component'
+import {LoginComponent} from './auth/components/login/login.component'
+import {DashboardComponent} from './features/dashboard/dashboard.component'
+import {InventarioComponent} from './features/inventario/inventario.component'
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, DashboardComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    InventarioComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -25,6 +31,7 @@ import { DashboardComponent } from './features/dashboard/dashboard.component'
     MatButtonModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
